67ab9f7e95ec890ba182730abe09d39b3496ce03
HamHarry <hamza_2641@hotmail.com>
2024-08-20 19:45:00 +0700

    api login

--- src/LoginPage/LoginPage.tsx
+++ src/LoginPage/LoginPage.tsx
@@ -1,16 +1,57 @@
+import { useState } from "react";
 import "./LoginPage.css";
+import axios from "axios";
+
 const LoginPage = () => {
+  const [valueInput, setValueInput] = useState<string>("");
+  const [valuePassword, setValuePassword] = useState<string>("");
+
+  //เก็บค่า input username and password =================================================================
+  const handleUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setValueInput(value);
+  };
+  const handlePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setValuePassword(value);
+  };
+
+  const handleSubmit = async (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    try {
+      e.preventDefault();
+      const item = {
+        username: valueInput,
+        password: valuePassword,
+      };
+      const res = await axios.post(
+        `https://www.melivecode.com/api/login`,
+        item
+      );
+      const userData = res.data;
+      console.log(userData);
+    } catch (error) {
+      alert("Username and password is wrong");
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container-login">
       <div className="warp-login">
         <h1>LOGIN</h1>
         <form>
           <h2>Username</h2>
-          <input type="text" placeholder="Username..." />
+          <input type="text" placeholder="Username..." onChange={handleUser} />
           <h2>Password</h2>
-          <input type="password" placeholder="Password..." />
+          <input
+            type="password"
+            placeholder="Password..."
+            onChange={handlePass}
+          />
           <div className="btn-login">
-            <div className="btn">
+            <div className="btn" onClick={handleSubmit}>
               <p>Log In</p>
             </div>
             <div className="btn">
