ad2798b6591b56282c6e837c2d2caa8fc2743ad2
HamHarry <hamza_2641@hotmail.com>
2024-08-22 16:00:29 +0700

    homepage complete

--- src/HomaPage/HomePage.tsx
+++ src/HomaPage/HomePage.tsx
@@ -11,18 +11,20 @@ const HomePage = () => {
         </div>
         <div className="container-table">
           <div className="table-game">
-            <div className="gird-table">
-              <div className="imagegame">
-                <img src="" alt="" />
-              </div>
-              <div className="namegame">
-                <h3>GTA</h3>
-              </div>
-              <div className="pricegame">
-                <h3>25 $</h3>
-              </div>
-              <div className="btn-buy">
-                <p>buy</p>
+            <div className="table">
+              <div className="gird-game">
+                <div className="imagegame">
+                  <img src="/public/assets/gta-v-main.jpg" alt="logo" />
+                </div>
+                <div className="namegame">
+                  <h3>Grand Theft Auto V</h3>
+                </div>
+                <div className="pricegame">
+                  <h3>25 $</h3>
+                </div>
+                <div className="btn-buy">
+                  <p>buy</p>
+                </div>
               </div>
             </div>
           </div>
