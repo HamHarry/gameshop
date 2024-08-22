ce53d53f173ad9590a65141309c6169bba3b8203
HamHarry <hamza_2641@hotmail.com>
2024-08-22 14:57:22 +0700

    homepage design

--- src/HomaPage/HomePage.tsx
+++ src/HomaPage/HomePage.tsx
@@ -5,7 +5,29 @@ const HomePage = () => {
   return (
     <div className="container-hompage">
       <Navbar />
-      <div>HomePage</div>
+      <div className="warp-container-homepage">
+        <div className="header">
+          <h1>Welcome Game Shop</h1>
+        </div>
+        <div className="container-table">
+          <div className="table-game">
+            <div className="gird-table">
+              <div className="imagegame">
+                <img src="" alt="" />
+              </div>
+              <div className="namegame">
+                <h3>GTA</h3>
+              </div>
+              <div className="pricegame">
+                <h3>25 $</h3>
+              </div>
+              <div className="btn-buy">
+                <p>buy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
