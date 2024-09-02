45e0a5385ee3a0963d3f063c79b89a785cc8ce84
HamHarry <hamza_2641@hotmail.com>
2024-09-02 13:11:00 +0700

    debit-card

--- src/PaymentPage/PaymentPage.tsx
+++ src/PaymentPage/PaymentPage.tsx
@@ -3,10 +3,37 @@ import Navbar from "../Navbar/Navbar";
 import "./PaymentPage.css";
 import "./DialogCard.css";
 import "./DialogQR.css";
+import "./Card.css";
+import { Controller, useForm } from "react-hook-form";
+
+interface Card {
+  number1: string;
+  number2: string;
+  number3: string;
+  number4: string;
+  fname: string;
+  lname: string;
+  date: string;
+  ccv: string;
+}
+
+const defaultValues: Card = {
+  number1: "",
+  number2: "",
+  number3: "",
+  number4: "",
+  fname: "",
+  lname: "",
+  date: "",
+  ccv: "",
+};
 
 const PaymentPage = () => {
   const [openDialogCard, setOpenDialogCard] = useState<boolean>(false);
   const [openDialogQR, setOpenDialogQR] = useState<boolean>(false);
+  const [cardForm, setCardForm] = useState<Card>();
+
+  const { handleSubmit, control } = useForm<Card>({ defaultValues });
 
   //render =================================================================
   const renderDialogCard = () => {
@@ -24,9 +51,183 @@ const PaymentPage = () => {
               }}
             ></i>
           </div>
-          <div className="warp-card"></div>
-          <div className="btn-celect">
-            <button>Celect</button>
+          <div className="warp-card">
+            <div className="card">
+              <div className="card-inner">
+                <div className="front">
+                  <img
+                    className="map-img"
+                    src="https://i.ibb.co/PYss3yv/map.png"
+                  />
+                  <div className="row">
+                    {/* width 60px */}
+                    <img src="https://i.ibb.co/G9pDnYJ/chip.png" />
+                    <img src="https://i.ibb.co/WHZ3nRJ/visa.png" />
+                  </div>
+                  <div className="row card-no">
+                    <p>{cardForm?.number1}</p>
+                    <p>{cardForm?.number2}</p>
+                    <p>{cardForm?.number3}</p>
+                    <p>{cardForm?.number4}</p>
+                  </div>
+                  <div className="row card-holder">
+                    <p>CARD HPLDER</p>
+                    <p>VALID TILL</p>
+                  </div>
+                  <div className="row name-card">
+                    <div className="fname-lname">
+                      <p>{cardForm?.fname}</p>
+                      <p>{cardForm?.lname}</p>
+                    </div>
+                    <p>{cardForm?.date}</p>
+                  </div>
+                </div>
+                <div className="back">
+                  <img
+                    className="map-img"
+                    src="https://i.ibb.co/PYss3yv/map.png"
+                  />
+                  <div className="bar"></div>
+                  <div className="row card-cvv">
+                    <div>
+                      <img src="https://i.ibb.co/S6JG8px/pattern.png" />
+                    </div>
+                    <p>{cardForm?.ccv}</p>
+                  </div>
+                  <div className="row card-text">
+                    <p>
+                      this card is a property of the Test Bank Public Company
+                      Limited.
+                    </p>
+                  </div>
+                  <div className="row signature">
+                    <p>CUSTOMER SIGNATURE</p>
+                    <img src="https://i.ibb.co/WHZ3nRJ/visa.png" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="cotainer-from-card">
+            <form onSubmit={handleSubmit(submit)}>
+              <div className="card-number">
+                <h3>Card Number</h3>
+                <div className="input-text-card-number">
+                  <Controller
+                    control={control}
+                    name="number1"
+                    render={({ field }) => {
+                      return (
+                        <input
+                          {...field}
+                          type="text"
+                          maxLength={4}
+                          placeholder="xxxx"
+                        />
+                      );
+                    }}
+                  />
+                  <Controller
+                    control={control}
+                    name="number2"
+                    render={({ field }) => {
+                      return (
+                        <input
+                          {...field}
+                          type="text"
+                          maxLength={4}
+                          placeholder="xxxx"
+                        />
+                      );
+                    }}
+                  />
+                  <Controller
+                    control={control}
+                    name="number3"
+                    render={({ field }) => {
+                      return (
+                        <input
+                          {...field}
+                          type="text"
+                          maxLength={4}
+                          placeholder="xxxx"
+                        />
+                      );
+                    }}
+                  />
+                  <Controller
+                    control={control}
+                    name="number4"
+                    render={({ field }) => {
+                      return (
+                        <input
+                          {...field}
+                          type="text"
+                          maxLength={4}
+                          placeholder="xxxx"
+                        />
+                      );
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="card-name">
+                <h3>Card Name</h3>
+                <div className="input-text-card-name">
+                  <Controller
+                    control={control}
+                    name="fname"
+                    render={({ field }) => {
+                      return (
+                        <input
+                          {...field}
+                          type="text"
+                          placeholder="FristName..."
+                        />
+                      );
+                    }}
+                  />
+                  <Controller
+                    control={control}
+                    name="lname"
+                    render={({ field }) => {
+                      return (
+                        <input
+                          {...field}
+                          type="text"
+                          placeholder="LastName..."
+                        />
+                      );
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="date-ccv">
+                <div className="date">
+                  <h3>Expiration Date</h3>
+                  <Controller
+                    control={control}
+                    name="date"
+                    render={({ field }) => {
+                      return <input {...field} type="month" />;
+                    }}
+                  />
+                </div>
+                <div className="ccv">
+                  <h3>CCV</h3>
+                  <Controller
+                    control={control}
+                    name="ccv"
+                    render={({ field }) => {
+                      return <input {...field} type="text" maxLength={3} />;
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="btn-celect">
+                <button type="submit">Celect</button>
+              </div>
+            </form>
           </div>
         </div>
       </dialog>
@@ -51,11 +252,26 @@ const PaymentPage = () => {
             <img src="/public/assets/qr-code.png" alt="logo" />
           </div>
           <div className="btn-complete">
-            <button>Complete</button>
+            <button
+              onClick={() => {
+                setOpenDialogQR(!openDialogQR);
+              }}
+            >
+              Complete
+            </button>
           </div>
         </div>
       </dialog>
     );
+  };
+
+  //handleSubmit =================================================================
+  const submit = (value: Card) => {
+    const item = {
+      ...value,
+    };
+    setCardForm(item);
+    setOpenDialogCard(!openDialogCard);
   };
 
   return (
