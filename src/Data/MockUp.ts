a3985a3eef61d09621bbccd53dec148eb3dbd645
HamHarry <hamza_2641@hotmail.com>
2024-09-13 12:17:37 +0700

    deploy vercel

--- src/Data/MockUp.ts
+++ src/Data/MockUp.ts
@@ -8,16 +8,16 @@ export const mockUp: GameItem[] = [
     image: "/assets/game/gta-v-main.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/GTA/GTA4.jpg",
+        image: "/assets/show/GTA/GTA4.jpg",
       },
       {
-        image: "/public/assets/show/GTA/GTA2.jpg",
+        image: "/assets/show/GTA/GTA2.jpg",
       },
       {
-        image: "/public/assets/show/GTA/GTA3.jpg",
+        image: "/assets/show/GTA/GTA3.jpg",
       },
       {
-        image: "/public/assets/show/GTA/GTA1.jpg",
+        image: "/assets/show/GTA/GTA1.jpg",
       },
     ],
     mode: "store",
@@ -26,19 +26,19 @@ export const mockUp: GameItem[] = [
     type: "open world",
     name: "Black desert",
     price: 200,
-    image: "/public/assets/game/Blackdesert.jpg",
+    image: "/assets/game/Blackdesert.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Blackdesert/Blackdesert3.jpg",
+        image: "/assets/show/Blackdesert/Blackdesert3.jpg",
       },
       {
-        image: "/public/assets/show/Blackdesert/Blackdesert2.jpg",
+        image: "/assets/show/Blackdesert/Blackdesert2.jpg",
       },
       {
-        image: "/public/assets/show/Blackdesert/Blackdesert1.jpg",
+        image: "/assets/show/Blackdesert/Blackdesert1.jpg",
       },
       {
-        image: "/public/assets/show/Blackdesert/Blackdesert4.png",
+        image: "/assets/show/Blackdesert/Blackdesert4.png",
       },
     ],
     mode: "store",
@@ -47,19 +47,19 @@ export const mockUp: GameItem[] = [
     type: "fps",
     name: "Valorant",
     price: 0,
-    image: "/public/assets/game/Valorant.jpg",
+    image: "/assets/game/Valorant.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Valorant/Valorant2.png",
+        image: "/assets/show/Valorant/Valorant2.png",
       },
       {
-        image: "/public/assets/show/Valorant/Valorant1.jpg",
+        image: "/assets/show/Valorant/Valorant1.jpg",
       },
       {
-        image: "/public/assets/show/Valorant/Valorant3.png",
+        image: "/assets/show/Valorant/Valorant3.png",
       },
       {
-        image: "/public/assets/show/Valorant/Valorant4.jpg",
+        image: "/assets/show/Valorant/Valorant4.jpg",
       },
     ],
     mode: "store",
@@ -68,19 +68,19 @@ export const mockUp: GameItem[] = [
     type: "fps",
     name: "Counter-Strike 2",
     price: 0,
-    image: "/public/assets/game/csgo.jpg",
+    image: "/assets/game/csgo.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Csgo/csgo1.jpg",
+        image: "/assets/show/Csgo/csgo1.jpg",
       },
       {
-        image: "/public/assets/show/Csgo/csgo2.jpg",
+        image: "/assets/show/Csgo/csgo2.jpg",
       },
       {
-        image: "/public/assets/show/Csgo/csgo3.jpeg",
+        image: "/assets/show/Csgo/csgo3.jpeg",
       },
       {
-        image: "/public/assets/show/Csgo/csgo4.jpg",
+        image: "/assets/show/Csgo/csgo4.jpg",
       },
     ],
     mode: "store",
@@ -89,19 +89,19 @@ export const mockUp: GameItem[] = [
     type: "fps",
     name: "Overwatch 2",
     price: 0,
-    image: "/public/assets/game/Overwatch 2.jpg",
+    image: "/assets/game/Overwatch 2.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Overwatch2/Overwatch4.jpg",
+        image: "/assets/show/Overwatch2/Overwatch4.jpg",
       },
       {
-        image: "/public/assets/show/Overwatch2/Overwatch2.png",
+        image: "/assets/show/Overwatch2/Overwatch2.png",
       },
       {
-        image: "/public/assets/show/Overwatch2/Overwatch3.jpg",
+        image: "/assets/show/Overwatch2/Overwatch3.jpg",
       },
       {
-        image: "/public/assets/show/Overwatch2/Overwatch1.jpg",
+        image: "/assets/show/Overwatch2/Overwatch1.jpg",
       },
     ],
     mode: "store",
@@ -110,19 +110,19 @@ export const mockUp: GameItem[] = [
     type: "open world",
     name: "Minecraft",
     price: 1340,
-    image: "/public/assets/game/Minecraft.jpg",
+    image: "/assets/game/Minecraft.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Minecraft/Minecraft1.jpg",
+        image: "/assets/show/Minecraft/Minecraft1.jpg",
       },
       {
-        image: "/public/assets/show/Minecraft/Minecraft2.jpg",
+        image: "/assets/show/Minecraft/Minecraft2.jpg",
       },
       {
-        image: "/public/assets/show/Minecraft/Minecraft3.jpg",
+        image: "/assets/show/Minecraft/Minecraft3.jpg",
       },
       {
-        image: "/public/assets/show/Minecraft/Minecraft4.jpg",
+        image: "/assets/show/Minecraft/Minecraft4.jpg",
       },
     ],
     mode: "store",
@@ -131,19 +131,19 @@ export const mockUp: GameItem[] = [
     type: "survive",
     name: "PUBG: BATTLEGROUNDS",
     price: 0,
-    image: "/public/assets/game/PUBG.jpg",
+    image: "/assets/game/PUBG.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/PUBG/PUBG4.png",
+        image: "/assets/show/PUBG/PUBG4.png",
       },
       {
-        image: "/public/assets/show/PUBG/PUBG2.jpg",
+        image: "/assets/show/PUBG/PUBG2.jpg",
       },
       {
-        image: "/public/assets/show/PUBG/PUBG3.jpg",
+        image: "/assets/show/PUBG/PUBG3.jpg",
       },
       {
-        image: "/public/assets/show/PUBG/PUBG1.jpg",
+        image: "/assets/show/PUBG/PUBG1.jpg",
       },
     ],
     mode: "store",
@@ -152,19 +152,19 @@ export const mockUp: GameItem[] = [
     type: "open world",
     name: "ARK: Survival Evolved",
     price: 315,
-    image: "/public/assets/game/ARK.jpg",
+    image: "/assets/game/ARK.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/ARK/ARK4.jpg",
+        image: "/assets/show/ARK/ARK4.jpg",
       },
       {
-        image: "/public/assets/show/ARK/ARK2.jpg",
+        image: "/assets/show/ARK/ARK2.jpg",
       },
       {
-        image: "/public/assets/show/ARK/ARK3.jpg",
+        image: "/assets/show/ARK/ARK3.jpg",
       },
       {
-        image: "/public/assets/show/ARK/ARK1.jpg",
+        image: "/assets/show/ARK/ARK1.jpg",
       },
     ],
     mode: "store",
@@ -173,19 +173,19 @@ export const mockUp: GameItem[] = [
     type: "open world",
     name: "RUST",
     price: 699,
-    image: "/public/assets/game/RUST.jpg",
+    image: "/assets/game/RUST.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/RUST/RUST4.png",
+        image: "/assets/show/RUST/RUST4.png",
       },
       {
-        image: "/public/assets/show/RUST/RUST2.jpg",
+        image: "/assets/show/RUST/RUST2.jpg",
       },
       {
-        image: "/public/assets/show/RUST/RUST3.jpg",
+        image: "/assets/show/RUST/RUST3.jpg",
       },
       {
-        image: "/public/assets/show/RUST/RUST1.jpg",
+        image: "/assets/show/RUST/RUST1.jpg",
       },
     ],
     mode: "store",
@@ -194,19 +194,19 @@ export const mockUp: GameItem[] = [
     type: "survive",
     name: "NARAKA: BLADEPOINT",
     price: 0,
-    image: "/public/assets/game/NARAKA.jpg",
+    image: "/assets/game/NARAKA.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/NARAKA/NARAKA4.jpg",
+        image: "/assets/show/NARAKA/NARAKA4.jpg",
       },
       {
-        image: "/public/assets/show/NARAKA/NARAKA2.jpg",
+        image: "/assets/show/NARAKA/NARAKA2.jpg",
       },
       {
-        image: "/public/assets/show/NARAKA/NARAKA3.jpg",
+        image: "/assets/show/NARAKA/NARAKA3.jpg",
       },
       {
-        image: "/public/assets/show/NARAKA/NARAKA1.jpg",
+        image: "/assets/show/NARAKA/NARAKA1.jpg",
       },
     ],
     mode: "store",
@@ -215,19 +215,19 @@ export const mockUp: GameItem[] = [
     type: "open world",
     name: "Black Myth: Wukong",
     price: 1799,
-    image: "/public/assets/game/Wukong.jpg",
+    image: "/assets/game/Wukong.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Wukong/preview4.jpg",
+        image: "/assets/show/Wukong/preview4.jpg",
       },
       {
-        image: "/public/assets/show/Wukong/preview2.jpg",
+        image: "/assets/show/Wukong/preview2.jpg",
       },
       {
-        image: "/public/assets/show/Wukong/preview3.jpg",
+        image: "/assets/show/Wukong/preview3.jpg",
       },
       {
-        image: "/public/assets/show/Wukong/preview1.jpg",
+        image: "/assets/show/Wukong/preview1.jpg",
       },
     ],
     mode: "store",
@@ -236,19 +236,19 @@ export const mockUp: GameItem[] = [
     type: "open world",
     name: "Cyberpunk",
     price: 1799,
-    image: "/public/assets/game/Cyberpunk.jpg",
+    image: "/assets/game/Cyberpunk.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Cyberpunk/Cyberpunk4.jpg",
+        image: "/assets/show/Cyberpunk/Cyberpunk4.jpg",
       },
       {
-        image: "/public/assets/show/Cyberpunk/Cyberpunk2.jpg",
+        image: "/assets/show/Cyberpunk/Cyberpunk2.jpg",
       },
       {
-        image: "/public/assets/show/Cyberpunk/Cyberpunk3.jpg",
+        image: "/assets/show/Cyberpunk/Cyberpunk3.jpg",
       },
       {
-        image: "/public/assets/show/Cyberpunk/Cyberpunk1.jpg",
+        image: "/assets/show/Cyberpunk/Cyberpunk1.jpg",
       },
     ],
     mode: "store",
@@ -257,19 +257,19 @@ export const mockUp: GameItem[] = [
     type: "open world",
     name: "Hogwarts Legacy",
     price: 1890,
-    image: "/public/assets/game/hogwarts.jpg",
+    image: "/assets/game/hogwarts.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/hogwarts/Hogwarts1.jpg",
+        image: "/assets/show/hogwarts/Hogwarts1.jpg",
       },
       {
-        image: "/public/assets/show/hogwarts/Hogwarts2.jpg",
+        image: "/assets/show/hogwarts/Hogwarts2.jpg",
       },
       {
-        image: "/public/assets/show/hogwarts/Hogwarts3.jpg",
+        image: "/assets/show/hogwarts/Hogwarts3.jpg",
       },
       {
-        image: "/public/assets/show/hogwarts/Hogwarts4.jpg",
+        image: "/assets/show/hogwarts/Hogwarts4.jpg",
       },
     ],
     mode: "store",
@@ -278,19 +278,19 @@ export const mockUp: GameItem[] = [
     type: "moba",
     name: "Dota 2",
     price: 0,
-    image: "/public/assets/game/dota2.jpg",
+    image: "/assets/game/dota2.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Dota/Dota2.jpg",
+        image: "/assets/show/Dota/Dota2.jpg",
       },
       {
-        image: "/public/assets/show/Dota/Dota1.png",
+        image: "/assets/show/Dota/Dota1.png",
       },
       {
-        image: "/public/assets/show/Dota/Dota3.jpg",
+        image: "/assets/show/Dota/Dota3.jpg",
       },
       {
-        image: "/public/assets/show/Dota/Dota4.jpg",
+        image: "/assets/show/Dota/Dota4.jpg",
       },
     ],
     mode: "store",
@@ -299,19 +299,19 @@ export const mockUp: GameItem[] = [
     type: "moba",
     name: "League of Legends",
     price: 0,
-    image: "/public/assets/game/lol.jpg",
+    image: "/assets/game/lol.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/LOL/lol1.jpg",
+        image: "/assets/show/LOL/lol1.jpg",
       },
       {
-        image: "/public/assets/show/LOL/lol2.jpg",
+        image: "/assets/show/LOL/lol2.jpg",
       },
       {
-        image: "/public/assets/show/LOL/lol3.jpg",
+        image: "/assets/show/LOL/lol3.jpg",
       },
       {
-        image: "/public/assets/show/LOL/lol4.jpg",
+        image: "/assets/show/LOL/lol4.jpg",
       },
     ],
     mode: "store",
@@ -320,19 +320,19 @@ export const mockUp: GameItem[] = [
     type: "survive",
     name: "Fortnite",
     price: 0,
-    image: "/public/assets/game/fortnite.jpg",
+    image: "/assets/game/fortnite.jpg",
     imageShow: [
       {
-        image: "/public/assets/show/Fortnite/fortnite4.jpg",
+        image: "/assets/show/Fortnite/fortnite4.jpg",
       },
       {
-        image: "/public/assets/show/Fortnite/fortnite3.jpeg",
+        image: "/assets/show/Fortnite/fortnite3.jpeg",
       },
       {
-        image: "/public/assets/show/Fortnite/fortnite1.jpg",
+        image: "/assets/show/Fortnite/fortnite1.jpg",
       },
       {
-        image: "/public/assets/show/Fortnite/fortnite2.jpg",
+        image: "/assets/show/Fortnite/fortnite2.jpg",
       },
     ],
     mode: "store",
@@ -341,19 +341,19 @@ export const mockUp: GameItem[] = [
     type: "survive",
     name: "Warface",
     price: 0,
-    image: "/public/assets/game/warface.png",
+    image: "/assets/game/warface.png",
     imageShow: [
       {
-        image: "/public/assets/show/Warface/warface3.jpg",
+        image: "/assets/show/Warface/warface3.jpg",
       },
       {
-        image: "/public/assets/show/Warface/warface2.jpg",
+        image: "/assets/show/Warface/warface2.jpg",
       },
       {
-        image: "/public/assets/show/Warface/warface4.jpg",
+        image: "/assets/show/Warface/warface4.jpg",
       },
       {
-        image: "/public/assets/show/Warface/warface1.jpg",
+        image: "/assets/show/Warface/warface1.jpg",
       },
     ],
     mode: "store",
@@ -362,23 +362,19 @@ export const mockUp: GameItem[] = [
     type: "survive",
     name: "Paladins Battlegrounds",
     price: 0,
-    image: "/public/assets/game/Paladins.jpg",
+    image: "/assets/game/Paladins.jpg",
     imageShow: [
       {
-        image:
-          "/public/assets/show/PaladinsBattlegrounds/PaladinsBattlegrounds1.jpg",
+        image: "/assets/show/PaladinsBattlegrounds/PaladinsBattlegrounds1.jpg",
       },
       {
-        image:
-          "/public/assets/show/PaladinsBattlegrounds/PaladinsBattlegrounds2.jpg",
+        image: "/assets/show/PaladinsBattlegrounds/PaladinsBattlegrounds2.jpg",
       },
       {
-        image:
-          "/public/assets/show/PaladinsBattlegrounds/PaladinsBattlegrounds3.jpg",
+        image: "/assets/show/PaladinsBattlegrounds/PaladinsBattlegrounds3.jpg",
       },
       {
-        image:
-          "/public/assets/show/PaladinsBattlegrounds/PaladinsBattlegrounds4.jpg",
+        image: "/assets/show/PaladinsBattlegrounds/PaladinsBattlegrounds4.jpg",
       },
     ],
     mode: "store",
